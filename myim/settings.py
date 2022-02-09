73d86a75b86d0f85bf7bde19e1885f90751c6764
wanazmi69 <lawtheoscar@gmail.com>
2022-02-09 21:41:00 +0900

    up commit

--- myim/settings.py
+++ myim/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_ROOT = [
+    BASE_DIR, 'staticfiles'
+]
 STATIC_URL = 'static/'
 STATICFILES_DIR = [
     BASE_DIR, 'static'
