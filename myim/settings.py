c6f21c40763f27030e84feb2991c7d4b076c9334
wanazmi69 <lawtheoscar@gmail.com>
2022-02-09 20:52:41 +0900

    up commit

--- myim/settings.py
+++ myim/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rrzy7j1n*+p3@jyjyu3^s_h39y=4g#7*ia!gor&*onyrg&3^t4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['myim.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
