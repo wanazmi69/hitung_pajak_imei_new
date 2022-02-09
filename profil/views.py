1215b94e1a699218a349bbf3cbe4573fb823ec2e
wanazmi69 <lawtheoscar@gmail.com>
2022-02-09 21:21:01 +0900

    up commit

---
+++ profil/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    context = {
+        'fProfile': 'img/profil.png',
+        'title': 'Profilku',
+        'judul': 'IMEI',
+        'nama': 'Wan Azmi',
+        'negara': 'Indonesia',
+        'tinggal': 'Osaka - Jepang',
+
+        'iconIG': 'img/media/ig.png'
+
+    }
+
+    return render(request, 'profil/index.html', context)
