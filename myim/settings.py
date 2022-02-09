f884adf1308a2887a7f6c076befd98bed3428fc1
wanazmi69 <lawtheoscar@gmail.com>
2022-02-09 21:26:19 +0900

    up commit

--- myim/settings.py
+++ myim/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [
+    BASE_DIR, 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
