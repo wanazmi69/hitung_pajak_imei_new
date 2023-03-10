e51e0309951cca857992aac5b0d488832b5d8fdb
wanazmi <lawtheoscar@gmail.com>
2023-03-11 05:36:51 +0900

    tes333hhk

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py tailwind install
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3 manage.py tailwind install
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
