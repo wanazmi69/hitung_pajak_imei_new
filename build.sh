5802449dd3b0b50ee6d107837180ad71785b402b
wanazmi <lawtheoscar@gmail.com>
2023-03-11 05:55:20 +0900

    tes againkoj

--- build.sh
+++ build.sh
@@ -2,15 +2,15 @@
 
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py tailwind install
 echo " BUILD END" 
 
 # # Build the project
 # echo "Building the project..."
 # python3 -m pip install -r requirements.txt
 
-# echo "Make Migration..."
 # python3 manage.py tailwind install
+# echo "Make Migration..."
 # python3 manage.py makemigrations --noinput
 # python3 manage.py migrate --noinput
 
