20d94c15aa6dd385b1388669ae67c4f57b08fd85
wanazmi <lawtheoscar@gmail.com>
2023-03-11 05:48:57 +0900

    tes again

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -52,7 +52,7 @@ TAILWIND_APP_NAME = 'theme'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 
     #  "django_browser_reload.middleware.BrowserReloadMiddleware",
 
